46d6b6f2932dd5bbbc148c13c8160624d3815b2c
aydindemir1 <aynndemr@gmail.com>
2024-03-11 20:46:47 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id = 1,BrandId=1,ColorId=1, DailyPrice =852,Description= "gndadsafdn",ModelYear=1995},
+                new Car { Id = 2,BrandId=2,ColorId=2, DailyPrice =4615,Description= "dfdasbd",ModelYear=2005},
+                new Car { Id = 3,BrandId=3,ColorId=3, DailyPrice =621,Description= "daddasb",ModelYear=2023},
+                new Car { Id = 4,BrandId=4,ColorId=4, DailyPrice =566,Description= "asdadasgdf",ModelYear=1999},
+                new Car { Id = 5,BrandId=5,ColorId=5, DailyPrice =753,Description= "sfrthhn",ModelYear=2020}
+            };
+
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c=>c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
